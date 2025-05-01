1c28510c0d267ccc277ef0f6a7793ca3e32a6f77
Ezra G <ezgolani@att.net>
2025-05-01 11:50:48 -0700

    first assignment

---
+++ Basic JavaScript Projects/Project1_Expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var A = "\"This is a string\""
++ " another string";
+
+var dad="dad", mom="mom", son="son", dog="dog"; 
+//These are printing different variables and mathematical values
+document.write(son)
+window.alert(A);
+document.write(3+3);
+
+var concatenated_String = dad + mom;
+//combining the above strings
+document.write(concatenated_String);
